a17cc199e6d4f11c8c6454faad007ab1b570ac0c
Koen Schoute <koen+git@gorgabal.nl>
2013-05-07 21:50:36 +0200

    Made a user interface mockup

---
+++ Development/IRI_UI/src/iri_ui/IRI_UI.java
@@ -0,0 +1,22 @@
+/*
+ * If you own this file you are free to edit and share it.
+ * You are, however, not allowed to mass-distribute it
+ * E.g.: Mailing a copy is O.K., 
+ * mirroring/hosting this or a modified version is not O.K.
+ * Exemptions to this are assigned at will by the author only.
+ */
+package iri_ui;
+
+/**
+ *
+ * @author Koen
+ */
+public class IRI_UI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
